760e5ddde2130d9446ad64915b41203f68d6a8a1
Ingu Bae <ingu.bae@gmail.com>
2019-02-13 15:57:32 +0900

    Add stringuitl.go file...

---
+++ stringutil.go
@@ -0,0 +1,10 @@
+package stringutil
+
+//"Reverse 인자 문자열을 rune-wise 방식으로 반전 리턴합니다."
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
